f5874635bd4c79afac28f9e94c6e9dd490e976f1
batsa-ike <batsa2010isaac@gmail.com>
2025-09-21 22:33:24 +0000

    vercel

--- auth-system/src/middleware/corsConfig.ts
+++ auth-system/src/middleware/corsConfig.ts
@@ -1,9 +1,24 @@
-import cors from 'cors';
+// src/middleware/corsConfig.ts
 
-const corsConfig = (isProduction: boolean) =>
-  cors({
-    origin: isProduction ? 'gimpa-dashboard-6vdcnbce7-batsaikes-projects.vercel.app/api/v1' : 'http://localhost:3000',
-    credentials: true,
-  });
+import cors, { CorsOptions } from 'cors';
+
+const allowedOrigins: string[] = [
+  'https://gimpa-dashboard.vercel.app',
+  'http://localhost:3000',
+  'https://gimpa-dashboard-6vdcnbce7-batsaikes-projects.vercel.app'
+];
+
+const corsOptions: CorsOptions = {
+  origin: (origin: string | undefined, callback: (err: Error | null, allow?: boolean) => void) => {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error('This origin is not allowed by CORS'));
+    }
+  },
+  credentials: true,
+};
+
+const corsConfig = () => cors(corsOptions);
 
 export default corsConfig;
